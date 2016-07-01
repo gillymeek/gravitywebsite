d521cc00b9686ce98117cdd44e952a3db7c11649
Stephen Voisey <stephen@pathfinder.agency>
2016-07-01 12:55:53 +0100

    Cosmetic fixes
    Equaliser added, header and footer gradient added for scroll fix, fixed
    placement of days in courses at smaller sizes.
    
    Also removed unwanted Foundation SCSS

--- themes/gravity/js/min/app-min.js
+++ themes/gravity/js/min/app-min.js
@@ -1,2 +1,1 @@
-$(document).foundation();
-//# sourceMappingURL=./app-min.js.map
+$(document).foundation(),$(document).ready(function(){$(function(){$(".mh-h").matchHeight(),$(".mh-b").matchHeight(),$(".mh-p1").matchHeight(),$(".mh-p2").matchHeight(),$(".mh-p3").matchHeight(),$(".mh-p4").matchHeight(),$(".mh-p5").matchHeight()})});
